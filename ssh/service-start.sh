569aeaab44518f033f9d06d548a66339304aff60
kalphageek <kalphageek@outlook.com>
2020-10-11 22:58:55 +0900

    modify service-start.sh

--- ssh/service-start.sh
+++ ssh/service-start.sh
@@ -9,4 +9,5 @@ echo "> TODAY : $TODAY"
 echo "> ORIGIN_JAR : $ORIGIN_JAR"
 
 #기존 버전으로 실행중인 프로세스에 영향을 주지 않기위해 applicaton.jar로부터 새로 등록된 jenkins-simple-batch-0.0.1-SNAPSHOT.jar 이름을 찾아와서 그것으로 실행한다.
-java -jar $ORIGIN_JAR --job.name=task1Job requestDate=$TODAY
+#java -jar $ORIGIN_JAR --job.name=task1Job requestDate=$TODAY
+java -jar $ORIGIN_JAR

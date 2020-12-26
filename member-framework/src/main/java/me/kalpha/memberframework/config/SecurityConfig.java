d393f7a4e203c2e3cda65caa3bff5e910eea7810
kalphageek <kalphageek@outlook.com>
2020-12-26 23:25:55 +0900

    member-framework 추가

--- member-framework/src/main/java/me/kalpha/memberframework/config/SecurityConfig.java
+++ member-framework/src/main/java/me/kalpha/memberframework/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package me.kalpha.memberframework.config;
 
+import me.kalpha.memberframework.controller.HomeController;
 import me.kalpha.memberframework.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,10 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+/**
+ * @EnableWebSecurity을 달면 SpringSecurityFilterChain이 자동으로 포함
+ * @EnableGlobalMethodSecurity(prePostEnabled = true)을 달면 {@link HomeController#adminView()}에서 @PreAuthorize("hasRole('ROLE_ADMIN')")가 동작함
+ */
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)

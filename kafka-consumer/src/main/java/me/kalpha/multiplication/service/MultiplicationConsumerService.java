ed701f40402ba398681a422285d56b9bb4599b31
kalphageek <kalphageek@outlook.com>
2021-02-21 00:34:27 +0900

    consumer에 의한 manual commit 추가

--- kafka-consumer/src/main/java/me/kalpha/multiplication/service/MultiplicationConsumerService.java
+++ kafka-consumer/src/main/java/me/kalpha/multiplication/service/MultiplicationConsumerService.java
@@ -2,14 +2,28 @@ package me.kalpha.multiplication.service;
 
 import lombok.extern.slf4j.Slf4j;
 import me.kalpha.multiplication.domain.MultiplicationSolvedEvent;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class MultiplicationConsumerService {
-    @KafkaListener(topics = "${app.topic.name}", groupId = "${app.group-id}")
-    public void consume(MultiplicationSolvedEvent multiplicationSolvedEvent) {
+    private final ConsumerFactory consumerFactory;
+    private KafkaConsumer<String, MultiplicationSolvedEvent> consumer;
+
+    @Autowired
+    public MultiplicationConsumerService(ConsumerFactory consumerFactory) {
+        this.consumerFactory = consumerFactory;
+        consumer = new KafkaConsumer<String, MultiplicationSolvedEvent>(consumerFactory.getConfigurationProperties());
+    }
+
+    @KafkaListener(topics = "${app.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(@Payload MultiplicationSolvedEvent multiplicationSolvedEvent) {
         log.info(String.format("$$$$ => Consumed message : %s", multiplicationSolvedEvent));
+        consumer.commitSync();
     }
 }

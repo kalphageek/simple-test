98c939f46d3544182ca5deff0becbc6b7ea09515
kalphageek <kalphageek@outlook.com>
2021-02-28 05:47:35 +0900

    common-config (공통모듈), test-app(공통모듈 테스트) 등록

--- common-config/src/main/java/me/kalpha/commonconfig/config/CommonConfig.java
+++ common-config/src/main/java/me/kalpha/commonconfig/config/CommonConfig.java
@@ -3,7 +3,6 @@ package me.kalpha.commonconfig.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 @Setter @Getter
 @ConfigurationProperties(prefix = "app.config")
--- common-config/src/main/java/me/kalpha/commonconfig/config/CommonConfigAutoConfiguration.java
+++ common-config/src/main/java/me/kalpha/commonconfig/config/CommonConfigAutoConfiguration.java
@@ -1,19 +1,11 @@
 package me.kalpha.commonconfig.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CommonConfigAutoConfiguration {
-//    private CommonConfig commonConfig;
-//
-//    @Autowired
-//    public CommonConfigAutoConfiguration(CommonConfig commonConfig) {
-//        this.commonConfig = commonConfig;
-//    }
-
     @ConditionalOnMissingBean
     @Bean
     public CommonConfig commonConfig() {

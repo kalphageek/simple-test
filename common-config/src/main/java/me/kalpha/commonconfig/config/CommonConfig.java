b6056325e69358e86475b62a0e9cf8b0e94ba432
kalphageek <kalphageek@outlook.com>
2021-03-01 09:38:45 +0900

    DataSize 사용 테스트

--- common-config/src/main/java/me/kalpha/commonconfig/config/CommonConfig.java
+++ common-config/src/main/java/me/kalpha/commonconfig/config/CommonConfig.java
@@ -3,9 +3,11 @@ package me.kalpha.commonconfig.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.util.unit.DataSize;
 
 @Setter @Getter
 @ConfigurationProperties(prefix = "app.config")
 public class CommonConfig {
     private String message;
+    private DataSize chickenSize;
 }

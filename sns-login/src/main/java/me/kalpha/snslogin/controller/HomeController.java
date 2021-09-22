33e9fe4343979d452fb9ab44574dbe9e06cec44d
kalphageek <kalphageek@outlook.com>
2021-09-22 17:33:24 +0900

    sns-login 추가

---
+++ sns-login/src/main/java/me/kalpha/snslogin/controller/HomeController.java
@@ -0,0 +1,46 @@
+package me.kalpha.snslogin.controller;
+
+import me.kalpha.snslogin.service.KakaoAPI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+
+@Controller
+public class HomeController {
+    @Autowired
+    private KakaoAPI kakao;
+
+    @RequestMapping(value="/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value="/login")
+    public String login(@RequestParam("code") String code, HttpSession session) {
+        String access_Token = kakao.getAccessToken(code);
+        HashMap<String, Object> userInfo = kakao.getUserInfo(access_Token);
+        System.out.println("login Controller : " + userInfo);
+
+        //    클라이언트의 이메일이 존재할 때 세션에 해당 이메일과 토큰 등록
+        if (userInfo.get("id") != null) {
+            session.setAttribute("userId", userInfo.get("id"));
+//            session.setAttribute("userId", userInfo.get("email"));
+            session.setAttribute("access_Token", access_Token);
+        }
+        return "index";
+    }
+
+    @RequestMapping(value="/logout")
+    public String logout(HttpSession session) {
+        kakao.kakaoLogout((String)session.getAttribute("access_Token"));
+        session.removeAttribute("access_Token");
+        session.removeAttribute("userId");
+        return "index";
+    }
+
+}
+

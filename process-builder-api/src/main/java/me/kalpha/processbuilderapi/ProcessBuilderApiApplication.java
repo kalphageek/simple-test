a714ab5c759e68e18b5aa811c6a1add96b753172
kalphageek <kalphageek@outlook.com>
2023-01-13 21:26:53 +0900

    process-builder-api 추가

---
+++ process-builder-api/src/main/java/me/kalpha/processbuilderapi/ProcessBuilderApiApplication.java
@@ -0,0 +1,13 @@
+package me.kalpha.processbuilderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessBuilderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessBuilderApiApplication.class, args);
+	}
+
+}
---
+++ process-builder-api/src/test/java/me/kalpha/processbuilderapi/ProcessBuilderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kalpha.processbuilderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessBuilderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

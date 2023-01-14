a49abcc2e284fefd6322493e365795ba0bc9700c
kalphageek <kalphageek@outlook.com>
2023-01-14 12:17:10 +0900

    CustomException 추가

--- process-builder-api/src/main/java/me/kalpha/processbuilderapi/common/Constants.java
+++ process-builder-api/src/main/java/me/kalpha/processbuilderapi/common/Constants.java
@@ -1,4 +1,24 @@
 package me.kalpha.processbuilderapi.common;
 
+import lombok.Getter;
+import lombok.ToString;
+
 public class Constants {
+    public enum ExceptionClass {
+        HANDLED("Handled"), UNHANDLED("Unhandled");
+
+        private String exceptionClass;
+
+        ExceptionClass(String exceptionClass) {
+            this.exceptionClass = exceptionClass;
+        }
+        public String getExceptionClass() {
+            return exceptionClass;
+        }
+
+        @Override
+        public String toString() {
+            return getExceptionClass() + " Exception.";
+        }
+    }
 }
--- process-builder-api/src/main/java/me/kalpha/processbuilderapi/common/GlobalExceptionHandler.java
+++ process-builder-api/src/main/java/me/kalpha/processbuilderapi/common/GlobalExceptionHandler.java
@@ -9,34 +9,53 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<Map<String,String>> unhandledException(Exception e) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+        log.error("Unhandled Exception 발생 : {}, {}", e.getCause(), e.getMessage());
+
+        Map<String, String> map = new HashMap<>();
+        map.put("error type", httpStatus.getReasonPhrase());
+        map.put("code", "400");
+        map.put("errorMessage", e.getMessage());
+
+        return new ResponseEntity<>(map, httpHeaders, httpStatus);
+    }
+
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<Map<String,String>> customException(CustomException e) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        log.error("Custom Exception 발생 : {}, {}", e.getHttpStatusType(), e.getMessage());
+
+        Map<String, String> map = new HashMap<>();
+        map.put("error type", e.getHttpStatusType());
+        map.put("code", Integer.toString(e.getHttpStatusCode()));
+        map.put("errorMessage", e.getMessage());
+
+        return new ResponseEntity<>(map, httpHeaders, e.getHttpStatus());
+    }
 
     @ExceptionHandler(CMDException.class)
     protected ResponseEntity<CMDResponse> handledException(CMDException e) {
         HttpHeaders httpHeaders = new HttpHeaders();
         HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
 
-        log.error("Handled Exception 발생 : {}", e.getErrorCode().getMessage());
+        log.error("Handled Exception 발생 : {}, {}", e.getCause(), e.getErrorCode().getMessage());
 
         CMDResponse cMDResponse = new CMDResponse();
         cMDResponse.setErrorMessage(e.getMessage());
         cMDResponse.setStatus(e.getErrorCode().getStatus());
-
-        return new ResponseEntity<CMDResponse>(cMDResponse, httpHeaders, httpStatus);
-    }
-    @ExceptionHandler(Exception.class)
-    protected ResponseEntity<CMDResponse> unhandledException(Exception e) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-
-        log.error("Unhandled Exception 발생 : {}", e.getMessage());
-
-        CMDResponse cMDResponse = new CMDResponse();
-        cMDResponse.setErrorMessage(e.getMessage());
-        cMDResponse.setStatus("500");
 
         return new ResponseEntity<CMDResponse>(cMDResponse, httpHeaders, httpStatus);
     }
